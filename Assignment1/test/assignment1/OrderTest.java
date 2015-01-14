51f3ead9dba81286a89e0dbc7d04a9962b505128
c0648442 <c0648442@SB-N208-12.lambton.on.ca>
2015-01-14 16:11:11 -0500

    Created Last 6 Test case by Kuldeep Singh

--- Assignment1/test/assignment1/OrderTest.java
+++ Assignment1/test/assignment1/OrderTest.java
@@ -227,7 +227,9 @@ public class OrderTest {
 
     @Test
     public void testWhenCustomerExistsAndPurchasedExistThenTimeReceivedIsNow() {
-
+     OrderTest orderQueue=new OrderTest();
+    
+     
     }
 
     @Test
